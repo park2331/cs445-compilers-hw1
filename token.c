250a29357e6761ae3d1c95e9d66f796a9f2f7c8e
Richard <park2331@vandals.uidaho.edu>
2014-09-19 16:03:11 -0700

    Fixed token to handle svals

--- token.c
+++ token.c
@@ -9,6 +9,39 @@ void print_token(tokenptr t) {
   printf("%d\t%s\t\t%d\t%s\t%d\t%s\n",t->category, t->text ,t->lineno, t->filename, t->ival, t->sval);
 };
 
+char *get_stringl(char *string) {
+  /* \n \t \' \\ \" and \0 */
+  int len = 0;
+  char* ptr = string;
+  char* strl = malloc(strlen(string)); 
+
+  /* Get past first quote */
+  while (*ptr++ != '\"') {
+    ptr++;
+  }
+
+  /* Get to end quote */
+  while (*ptr != '\"') {
+    if (ptr[0] == '\\') {
+      char esc = ptr[1];
+      //      printf("Found escape char: %c%c\n",ptr[0],esc);
+      if (esc == 'n' || esc == 't' || esc == '\'' || esc == '\\' || esc == '\"' || esc == '\0') {
+	ptr++;
+	ptr++;
+      } 
+    } else {
+      strl[len] = *ptr;
+      len++;
+      ptr++;
+    }
+  }
+  strl[len] = '\0';
+  strl = realloc(strl, len);
+  return strl;
+}
+
+
+
 tokenptr create_token(int category, char *text, int lineno, char *filename) {
   tokenptr t = malloc(sizeof(struct token));
 
@@ -19,6 +52,7 @@ tokenptr create_token(int category, char *text, int lineno, char *filename) {
   t->category = category;
 
   /* Actual string matched */
+  /* memory leak? */
   t->text = malloc(strlen(text)+1);
   strcpy(t->text, text);
 
@@ -33,9 +67,9 @@ tokenptr create_token(int category, char *text, int lineno, char *filename) {
     t->ival = atoi(text);
   }
 
+  /* memory leak? */
   if (category == STRING) {
-    t->sval = malloc(strlen(text)+1);
-    strcpy(t->sval, text);
+    t->sval = get_stringl(text);
   }
   return t;
 };
